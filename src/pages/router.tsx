077b695e98eadabee7fda729cc9e6826d2bafca3
Alejandro Rodríguez López <arodriguezl9302@gmail.com>
2022-02-26 02:50:22 +0100

    feat: add-500-error-page

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ErrorBoundary from '../components/ErrorBoundary'
 import Home from './Home'
 import Error404 from './Error404'
+import Error500 from './Error500'
 
 const Router = () => (
   <ErrorBoundary>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/internal-error" element={<Error500 />} />
         <Route path="/*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
